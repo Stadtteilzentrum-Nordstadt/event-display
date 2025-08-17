f9edb1be90839061440618c3ea70c6ed415decf8
Emma Böcker <emma@boecker.dev>
2025-08-17 16:54:00 +0200

    force /wochenplan to render dynamically

--- src/app/wochenplan/page.tsx
+++ src/app/wochenplan/page.tsx
@@ -2,6 +2,8 @@ import { loadConfig } from "../loadConfig";
 import Header from "./header";
 import WeekDisplayRoot from "./week-display-root";
 
+export const dynamic = "force-dynamic";
+
 export default async function WochenplanPage() {
   const config = await loadConfig();
 
